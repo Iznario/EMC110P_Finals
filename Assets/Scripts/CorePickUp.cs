7d8c122d94a118842cbb645bee2f066d267b13e5
Iznario <miguihlopez@gmail.com>
2020-11-07 22:07:03 +0800

    first commit

---
+++ Assets/Scripts/CorePickUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CorePickUp : MonoBehaviour
+{
+    public GameObject ScoreText;
+    public int theScore;
+    public AudioSource collectSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        collectSound.Play();
+        theScore += 10;
+        ScoreText.GetComponent<Text>().text= "Score: " + theScore;
+        Destroy(gameObject);
+    }
+}
